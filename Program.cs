b1dd1bd476203010dfd664b0debe30ce2607d112
poojakrp777 <poojakrp777@gmail.com>
2022-09-14 22:11:54 +0530

    lengthbetween two line

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        public class Pair
+        {
+            public int first, second;
+
+            public Pair(int first, int second)
+            {
+                this.first = first;
+                this.second = second;
+            }
+        }
+
+        // Function to find the line given two points
+        static void lineFromPoints(Pair P, Pair Q)
+        {
+            int a = Q.second - P.second;
+            int b = P.first - Q.first;
+            int c = a * (P.first) + b * (P.second);
+
+            if (b < 0)
+            {
+                Console.WriteLine("The line passing through points P and Q is: "+ a + "x - " + b + "y = " + c);
+            }
+            else
+            {
+                Console.WriteLine("The line passing through points P and Q is: "+ a + "x + " + b + "y = " + c);
+            }
+        }
+
+        // Driver code
+        public static void Main(String[] args)
+        {
+            Pair P = new Pair(3, 2);
+            Pair Q = new Pair(2, 6);
+            lineFromPoints(P, Q);
+        }
+    }
+}
